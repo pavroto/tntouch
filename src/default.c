23f599148ffc460172879c77901fa2ba7b155731
Pavel Rotov <pavel.rotovv@gmail.com>
2024-11-13 14:41:49 +0200

    src/: formatted with clang-format

--- src/default.c
+++ src/default.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #define DEFAULT_CONFIG_DIR "/.config"
 #define PROJECT_DIR "/tntouch"
@@ -12,160 +12,153 @@
 
 // normalize_path:
 // Removes repeating `/` characters from path.
-char*
-normalize_path
-(char* path, int length)
-{ 
+char *
+normalize_path (char *path, int length)
+{
   char buf;
-  char* output = (char*)malloc(length);
-  
+  char *output = (char *)malloc (length);
+
   int k = 0;
   for (int i = 0; path[i] != '\0' && i < length; i++)
-  {
-    if (buf && buf == '/' && path[i] == '/')
-      continue;
+    {
+      if (buf && buf == '/' && path[i] == '/')
+        continue;
 
-    if (path[i] == '\n')
-      continue;
+      if (path[i] == '\n')
+        continue;
 
-    output[k++] = path[i];
-    buf = path[i];
-  }
+      output[k++] = path[i];
+      buf = path[i];
+    }
   output[k] = '\0';
-  
-  free(path);
+
+  free (path);
   return output;
 }
 
 // get_cpath:
 // Returns path to the `default` file containing
 // path to the default template.
-char* 
-get_cpath
-(void)
+char *
+get_cpath (void)
 {
-	char* xdg_cpath = getenv("XDG_CONFIG_HOME");
-	if (xdg_cpath != NULL)
-	{
-		size_t len = 
-			strlen(xdg_cpath) +
-			strlen(PROJECT_DIR) +
-			strlen(CONFIG_NAME) + 1;
+  char *xdg_cpath = getenv ("XDG_CONFIG_HOME");
+  if (xdg_cpath != NULL)
+    {
+      size_t len = strlen (xdg_cpath) + strlen (PROJECT_DIR)
+          + strlen (CONFIG_NAME) + 1;
 
-		char* cpath = (char*)calloc(len, sizeof(char));
-		if (cpath == NULL)
-			return NULL;
-	
-		snprintf(cpath, len, "%s%s%s", xdg_cpath, PROJECT_DIR, CONFIG_NAME);
+      char *cpath = (char *)calloc (len, sizeof (char));
+      if (cpath == NULL)
+        return NULL;
 
-    cpath = normalize_path(cpath, len);
+      snprintf (cpath, len, "%s%s%s", xdg_cpath, PROJECT_DIR, CONFIG_NAME);
 
-		return cpath;
-	}
+      cpath = normalize_path (cpath, len);
 
-	char* hpath = getenv("HOME");
-	if (hpath == NULL)
-		return NULL;	
+      return cpath;
+    }
 
-	size_t len = 
-		strlen(hpath) + 
-		strlen(DEFAULT_CONFIG_DIR) + 
-		strlen(PROJECT_DIR) +
-		strlen(CONFIG_NAME) + 1;
+  char *hpath = getenv ("HOME");
+  if (hpath == NULL)
+    return NULL;
 
-	char* cpath = (char*)calloc(len, sizeof(char));
-	if (cpath == NULL)
-		return NULL;
+  size_t len = strlen (hpath) + strlen (DEFAULT_CONFIG_DIR)
+      + strlen (PROJECT_DIR) + strlen (CONFIG_NAME) + 1;
 
-	snprintf(cpath, len, "%s%s%s%s", hpath, DEFAULT_CONFIG_DIR, PROJECT_DIR, CONFIG_NAME);
+  char *cpath = (char *)calloc (len, sizeof (char));
+  if (cpath == NULL)
+    return NULL;
 
-  cpath = normalize_path(cpath, len);
+  snprintf (cpath, len, "%s%s%s%s", hpath, DEFAULT_CONFIG_DIR, PROJECT_DIR,
+            CONFIG_NAME);
 
-	return cpath;
+  cpath = normalize_path (cpath, len);
+
+  return cpath;
 }
 
 // get_ptemplate:
 // Find template using provided `path` and return
 // its content.
-char*
-get_ptemplate
-(const char *path)
+char *
+get_ptemplate (const char *path)
 {
-  FILE* tfile = fopen(path, "r");
+  FILE *tfile = fopen (path, "r");
   if (tfile == NULL)
-  {
-    perror(path);
-    return NULL;
-  }
+    {
+      perror (path);
+      return NULL;
+    }
 
-  char* template = (char*)malloc(MAX_TEMPLATE_LENGTH);
-  int template_size = fread(template, sizeof(char), MAX_TEMPLATE_LENGTH-1, tfile);
-  fclose(tfile);
+  char *template = (char *)malloc (MAX_TEMPLATE_LENGTH);
+  int template_size
+      = fread (template, sizeof (char), MAX_TEMPLATE_LENGTH - 1, tfile);
+  fclose (tfile);
 
   if (template_size < 1)
-  {
-    fprintf(stderr, "%s: Template is invalid\n", path);
-    free(template);
-    return NULL;
-  }
+    {
+      fprintf (stderr, "%s: Template is invalid\n", path);
+      free (template);
+      return NULL;
+    }
 
   return template;
 }
 
 // set_dtemplate:
 // Set new default template.
-int 
-set_dtemplate
-(const char *path)
+int
+set_dtemplate (const char *path)
 {
-	// TODO: set_dtemplate
-	//
-	// 1. Check if file exists
-	// 2. Check if template is valid (maybe?)
-	// 3. Set new default template
-   
+  // TODO: set_dtemplate
+  //
+  // 1. Check if file exists
+  // 2. Check if template is valid (maybe?)
+  // 3. Set new default template
+
   return 1;
 }
 
 // get_dtemplate:
 // Find default template and return its content.
-char* 
-get_dtemplate
-(void)
-{	
-	char *cpath = get_cpath();
-	
-	FILE* cfile = fopen(cpath, "r");
-	if (cfile == NULL)
-	{
-		perror(cpath);
-		free(cpath);
-		return NULL;
-	}	
+char *
+get_dtemplate (void)
+{
+  char *cpath = get_cpath ();
 
-	char* tpath = (char*)malloc(MAX_TEMPLATE_LENGTH);
-	
- 	int tpath_size = fread(tpath, sizeof(char), MAX_TEMPLATE_LENGTH-1, cfile);
-	fclose(cfile);
+  FILE *cfile = fopen (cpath, "r");
+  if (cfile == NULL)
+    {
+      perror (cpath);
+      free (cpath);
+      return NULL;
+    }
 
-	if (tpath_size <= 1)
-  {
-    fprintf(stderr, "Default template path is invalid.\n");
-    free(tpath);
-    free(cpath);
-		return NULL;
-  }
+  char *tpath = (char *)malloc (MAX_TEMPLATE_LENGTH);
 
-  tpath = normalize_path(tpath, strlen(tpath));
-  char* template = get_ptemplate(tpath);
+  int tpath_size
+      = fread (tpath, sizeof (char), MAX_TEMPLATE_LENGTH - 1, cfile);
+  fclose (cfile);
+
+  if (tpath_size <= 1)
+    {
+      fprintf (stderr, "Default template path is invalid.\n");
+      free (tpath);
+      free (cpath);
+      return NULL;
+    }
+
+  tpath = normalize_path (tpath, strlen (tpath));
+  char *template = get_ptemplate (tpath);
   if (template == NULL)
-  {
-    free(cpath);
-    free(tpath);
-    return NULL;
-  }
+    {
+      free (cpath);
+      free (tpath);
+      return NULL;
+    }
 
-  free(cpath);
-  free(tpath);
+  free (cpath);
+  free (tpath);
   return template;
 }
--- src/tntouch.c
+++ src/tntouch.c
@@ -1,97 +1,96 @@
 // SPDX-License-Identifier: MIT
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "default.h"
 
-void show_help_text(void);
+void show_help_text (void);
 
-int 
-main
-(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-	int opt;
-	int hflag = 0;
-	char* dvalue = NULL;
-	char* tvalue = NULL;
-  char* ivalue = NULL;
-	
-	while ((opt = getopt(argc, argv, "hd:t:")) != -1)
-	{
-		switch (opt)
-		{
-			// h: Show help page;
-			case 'h':
-				hflag = 1;
-				break;
+  int opt;
+  int hflag = 0;
+  char *dvalue = NULL;
+  char *tvalue = NULL;
+  char *ivalue = NULL;
 
-			// d: Specify default template;
-			case 'd':
-				dvalue = strdup(optarg);
-				break;
+  while ((opt = getopt (argc, argv, "hd:t:")) != -1)
+    {
+      switch (opt)
+        {
+        // h: Show help page;
+        case 'h':
+          hflag = 1;
+          break;
 
-			// t: Specify template path;
-			case 't':
-				tvalue = strdup(optarg);
-				break;
+        // d: Specify default template;
+        case 'd':
+          dvalue = strdup (optarg);
+          break;
 
-			default:
-        show_help_text();
-        return 1;
-		}
-	}
+        // t: Specify template path;
+        case 't':
+          tvalue = strdup (optarg);
+          break;
 
-	if (hflag)
-	{
-		show_help_text();
-		return 0;
-	}
+        default:
+          show_help_text ();
+          return 1;
+        }
+    }
+
+  if (hflag)
+    {
+      show_help_text ();
+      return 0;
+    }
 
   if (argc != 1)
-  {
-    char ifpdash = 0;
-    for (int i = 1; i < argc; i++)
     {
-      if (argv[i][0] == '-')
-      {
-        ifpdash = 1;
-        continue;
-      }
-      else if (ifpdash)
-      {
-        ifpdash = 0;
-        continue;
-      }
-      ivalue = argv[i];
-      break;
+      char ifpdash = 0;
+      for (int i = 1; i < argc; i++)
+        {
+          if (argv[i][0] == '-')
+            {
+              ifpdash = 1;
+              continue;
+            }
+          else if (ifpdash)
+            {
+              ifpdash = 0;
+              continue;
+            }
+          ivalue = argv[i];
+          break;
+        }
     }
-  }
-	printf("hflag: %d\ndvalue: %s\ntvalue: %s\nivalue: %s\n", hflag, dvalue, tvalue, ivalue); 	
+  printf ("hflag: %d\ndvalue: %s\ntvalue: %s\nivalue: %s\n", hflag, dvalue,
+          tvalue, ivalue);
 
-	// all good;
-	return 0;
+  // all good;
+  return 0;
 }
 
-void 
-show_help_text
-(void)
+void
+show_help_text (void)
 {
-	static const char *help_text = \
-		"Usage: tntouch [OPTION]... PATH [VARIABLE]...\n"
-		"Use a template name for new files.\n"
-		"Example: tntouch -t /path/to/template Foo\n"
-		"\n"
-		"Options:\n"
-		"-t   Select a template.\n"
-		"-d   Select a default template.\n"
-		"-h   Show this help page.\n"
-		"\n"
-		"Report bugs to: <https://github.com/pavroto/tntouch/issues>\n"
-		"Tntouch home page: <https://github.com/pavroto/tntouch>\n";
+  static const char *help_text
+      = "Usage: tntouch [OPTION]... PATH [VARIABLE]...\n"
+        "Use a template name for new files.\n"
+        "Example: tntouch -t /path/to/template Foo\n"
+        "\n"
+        "Options:\n"
+        "-t   Select a template.\n"
+        "-d   Select a default template.\n"
+        "-h   Show this help page.\n"
+        "\n"
+        "Report bugs to: <https://github.com/pavroto/tntouch/issues>\n"
+        "Tntouch home page: <https://github.com/pavroto/tntouch>\n";
 
-	printf(help_text);
+  printf (help_text);
 }
